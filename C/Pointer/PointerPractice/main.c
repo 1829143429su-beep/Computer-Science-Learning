61f73e6d9dab0664c1041c35fe0b5ebade8ebf56
suyan <1829143429su@gmail.com>
2026-08-24 11:47:43 +0800

    Create new projects

--- C/Pointer/PointerPractice/main.c
+++ C/Pointer/PointerPractice/main.c
@@ -198,8 +198,237 @@ void test_array_reverse()
     }   
 }
 
+//数组实现冒泡排序(升序)
+void test_array_buuble_sort()
+{
+    int arr[5] = { 3,8,2,10,5 };
+   
+    for (int i = 0; i < 4; i++) {//4轮
+        for (int j = 0; j < 4-i; j++) {//4项 有下一项
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j+ 1] = temp;
+            }
+        }
+    }
+    for (int k = 0; k < 5; k++) {
+        printf("%d ", arr[k]);
+    }
+}
+//指针实现冒泡排序(升序)
+void test_pointer_buuble_sort()
+{
+    int arr[5] = { 3,8,2,10,5 };
+    for (int i = 0; i < 4; i++) {//4轮
+        int* p = arr;
+        while(p<arr+4-i) {//4项 有下一项
+            if (*p > *(p+1) ){
+                int temp = *p;
+                *p = *(p+1);
+                *(p+1)= temp;
+            }
+            p++;
+        }
+    }
+    int* p = arr;
+    while (p < arr + 5) {
+        printf("%d", *p);
+        p++;
+    }
+}
+
+//指针与函数：
+//1.传值交换
+void value_swap(int a, int b) {
+    int temp = a;
+    a = b;
+    b = temp;
+    printf("a=%d,b=%d\n", a, b);
+}
+void result1()
+{
+    int x = 5, y = 10;
+    value_swap(x, y);
+    printf("x=%d,y=%d\n", x, y);
+}
+//传址交换（指针）
+void address_swap(int* a,int*b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+    printf("*a=%d,*b=%d\n", *a, *b);
+}
+void result2()
+{ 
+    int x = 5, y = 10;
+    address_swap(&x, &y);
+    printf("x=%d,y=%d\n", x, y);
+}
+
+//2.指针作为函数参数求数组最大值
+int array_max(int* arr, int size) {
+    int max = *arr;
+    for (int i = 1; i < size; i++) {
+        if (*(arr+i) > max) {
+            max = *(arr+i);
+        }
+    }
+    return max;
+}
+//最小值
+int array_min(int* arr, int size)
+{
+    int min = *arr;
+    for (int i = 1; i < size; i++) {
+        if (*(arr + i) < min) {
+            min = *(arr + i);
+        }
+    }
+    return min;
+}
+void result() {
+    int arr[5] = { 3,2,10,5,8 };
+    int size = 5;
+    int result3 = array_max(arr, 5);
+    int result4 = array_min(arr, 5);
+    printf("最大值为：%d\n最小值是：%d", result3,result4);
+}
+
+//3.排序
+void array_sort(int* arr, int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i-1; j++) {
+            if (*(arr + j) > *(arr + j + 1)) {
+                int temp = *(arr+j);
+                *(arr + j) = *(arr + j + 1);
+                *(arr + j + 1) = temp;
+            }
+        }
+    }
+}
+void result3() {
+    int arr[5] = { 3,2,10,5,8 };
+    int size = 5;
+    array_sort(arr, 5);//排序
+    for (int i = 0; i < size; i++) {
+        printf("%d ", *(arr + i));//输出
+    }
+}
+
+//4.数组逆序
+void array_reverse(int* arr, int size) {
+    for (int i = 0; i < size / 2 ; i++) {
+        int temp = *(arr + i);
+        *(arr + i) = *(arr + size-i-1);
+        *(arr + size-i-1) = temp;
+    }
+}
+void result4() {
+    int arr[5] = { 3,2,10,5,8 };
+    int size = 5;
+    array_reverse(arr, 5);//逆序
+    for(int i = 0; i < size; i++) {
+        printf("%d ", *(arr + i));//输出
+    }
+}
+
+
+//指针与函数
+//1.自定义字符串长度函数
+int my_strlen(char* str)
+{
+    int count = 0;
+    while (*str !='\0')/* 字符不是 \0 */
+    {
+        count++;
+        str++;
+    }
+    return count;
+}
+int result5() {
+    char str[] = "hello";
+    int len = my_strlen(str);
+    printf("长度：%d\n", len);
+}
+
+//2.字符串复制函数
+void my_strcpy(char*dest,char*src)
+{
+    while (*src!='\0')
+    {
+        *dest = *src;
+        dest++;
+        src++;
+    }
+    *dest = '\0';
+}
+void result6() {
+    char* dest[20];//初始化
+    char* src = "hello";
+    my_strcpy(dest, src);
+    printf("复制后的字符串：%s\n", dest);
+}
+
+//3.字符串比较函数
+int my_strcmp(char* str1, char* str2)
+{
+    while (*str1 != '\0' && *str2 != '\0') {
+        if (*str1 == *str2) {
+            str1++;
+            str2++;
+        }
+        else {
+            break;
+        }
+        
+    }
+    if (*str1 == *str2) {
+        return 0;
+    }
+    else if (*str1 > *str2) {
+        return 1;//不输出，只比较
+       //return已经结束函数了，不需要break
+    }
+    else {
+        return -1;
+    }
+}
+void result7() {
+    char* str1 = "abe";
+    char* str2 = "abd";
+    int result = my_strcmp(str1, str2);
+    if (result == 0) {
+        printf("字符串相等\n");
+    }else if (result == 1) {
+        printf("str1>str2\n");
+    }else if (result == -1) {
+        printf("str1<str2\n");
+    }
+}
+
+//4.字符串连接函数
+void my_strcat(char* dest, char* src)
+{
+    while (*dest != '\0') {
+        dest++;
+    }
+    while (*src != '\0') {
+        *dest = *src;
+        dest++;
+        src++;
+    }
+    *dest = '\0';
+}
+void result8() {
+    char dest[20] = "hello";
+    char* src = "world";
+    my_strcat(dest, src);//void
+    printf("拼接后的函数是：%s\n", dest);
+}
 int main()
 {
-    test_array_reverse();
+    result8();
     return 0;
 }
--- C/Pointer/PointerPractice/综合练习.c
+++ C/Pointer/PointerPractice/综合练习.c
@@ -1,2 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS
 
+#include<stdio.h>
+
+//输出两个字符串长度
+int my_strlen(char* dest) {
+	int i = 0;
+	while (*dest != 0) {
+		i++;
+		dest++;
+	}//不包括'\0'
+}
+
+
+
+int main() {
+	//输入两个字符串
+	char* dest = "hello";
+	char* src = "world";
+	char* str;
+    //输出两个字符串长度
+	printf("字符串长度分别是%zu,%zu", strlen(dest),strlen(src);
+	int length1 = my_strlen(dest);
+	int length2 = my_strlen(src);
+	printf("字符串长度分别是%d,%d", length1, length2);
+
+   //复制第一个字符串
+    
+比较两个字符串
+↓
+把两个字符串拼接起来
+↓
+输出最终结果
+void 
 	
